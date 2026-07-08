a91fe0ee874c141190ccfe8a8da6ac537ac79b95
anil shrestha <stha.anill2059@gmail.com>
2026-07-08 16:51:07 +0545

    Explore dataset structure

--- src/load_data.py
+++ src/load_data.py
@@ -1,6 +1,12 @@
 import pandas as pd
-# Read the dataset
+# Load the dataset
 matches = pd.read_csv("data/raw/results.csv")
 
 # Show first 5 rows
 print(matches.head())
+
+print("\nDataset Shape:")
+print(matches.shape)
+
+print("\nColumn Names:")
+print(matches.columns)
