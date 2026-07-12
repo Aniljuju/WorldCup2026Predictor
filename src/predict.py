012c8c0cc36403a6a8954487b99b8fb2bb5a9989
anil shrestha <stha.anill2059@gmail.com>
2026-07-12 23:07:58 +0545

    Add initial match prediction input

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,15 @@
 import joblib
 
-# Load the trained model
+# Load model
 model = joblib.load("models/random_forest.pkl")
 
-print("Model loaded successfully!")
+# Load encoders
+team_encoder = joblib.load("models/team_encoder.pkl")
+winner_encoder = joblib.load("models/winner_encoder.pkl")
+
+print("✅ Model loaded successfully!")
+print("✅ Team encoder loaded successfully!")
+print("✅ Winner encoder loaded successfully!")
+
+home_team = input("Enter Home Team: ")
+away_team = input("Enter Away Team: ")
