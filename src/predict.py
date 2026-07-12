0673249484d4311c3d12078f4c3d042255544817
anil shrestha <stha.anill2059@gmail.com>
2026-07-12 23:22:36 +0545

    Implement match prediction using trained model

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,5 @@
 import joblib
-
+import pandas as pd
 # Load model
 model = joblib.load("models/random_forest.pkl")
 
@@ -21,3 +21,26 @@ away_encoded = team_encoder.transform([away_team])[0]
 print("\nEncoded Teams")
 print(f"{home_team} -> {home_encoded}")
 print(f"{away_team} -> {away_encoded}")
+
+# Create input for the model
+match = pd.DataFrame(
+    [[home_encoded, away_encoded]],
+    columns=["home_team", "away_team"]
+)
+
+# Predict the winner
+prediction = model.predict(match)
+
+# Decode prediction
+result = winner_encoder.inverse_transform(prediction)[0]
+
+print("\n🏆 Match Prediction")
+print(f"Home Team : {home_team}")
+print(f"Away Team : {away_team}")
+
+if result == "Home":
+    print(f"\nWinner: {home_team}")
+elif result == "Away":
+    print(f"\nWinner: {away_team}")
+else:
+    print("\nWinner: Draw")
