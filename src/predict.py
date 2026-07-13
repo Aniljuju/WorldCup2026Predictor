8102b6c014ea9764ae42263c0d0bb5e1add841f3
anil shrestha <stha.anill2059@gmail.com>
2026-07-13 12:45:43 +0545

    Refactor Prediction into usable function

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,6 @@
 import joblib
 import pandas as pd
+
 # Load model
 model = joblib.load("models/random_forest.pkl")
 
@@ -11,44 +12,44 @@ print("✅ Model loaded successfully!")
 print("✅ Team encoder loaded successfully!")
 print("✅ Winner encoder loaded successfully!")
 
-home_team = input("Enter Home Team: ")
-away_team = input("Enter Away Team: ")
 
-# Encode the team names
-home_encoded = team_encoder.transform([home_team])[0]
-away_encoded = team_encoder.transform([away_team])[0]
+def predict_match(home_team, away_team):
+    # Encode the team names
+    home_encoded = team_encoder.transform([home_team])[0]
+    away_encoded = team_encoder.transform([away_team])[0]
 
-print("\nEncoded Teams")
-print(f"{home_team} -> {home_encoded}")
-print(f"{away_team} -> {away_encoded}")
+    # Create input for the model
+    match = pd.DataFrame(
+        [[home_encoded, away_encoded]],
+        columns=["home_team", "away_team"]
+    )
 
-# Create input for the model
-match = pd.DataFrame(
-    [[home_encoded, away_encoded]],
-    columns=["home_team", "away_team"]
-)
+    # Predict the winner
+    prediction = model.predict(match)
+    probabilities = model.predict_proba(match)
 
-# Predict the winner
-prediction = model.predict(match)
-probabilities = model.predict_proba(match)
+    print("\nPrediction Probabilities:")
+    for label, prob in zip(winner_encoder.classes_, probabilities[0]):
+        print(f"{label}: {prob:.2%}")
 
-print("\nClasses:")
-print(winner_encoder.classes_)
+    # Decode prediction
+    result = winner_encoder.inverse_transform(prediction)[0]
 
-print("\nPrediction Probabilities:")
-for label, prob in zip(winner_encoder.classes_, probabilities[0]):
-    print(f"{label}: {prob:.2%}")
+    if result == "Home":
+        return home_team
+    elif result == "Away":
+        return away_team
+    else:
+        return "Draw"
 
-# Decode prediction
-result = winner_encoder.inverse_transform(prediction)[0]
 
-print("\n🏆 Match Prediction")
-print(f"Home Team : {home_team}")
-print(f"Away Team : {away_team}")
+if __name__ == "__main__":
+    home_team = input("Enter Home Team: ")
+    away_team = input("Enter Away Team: ")
 
-if result == "Home":
-    print(f"\nWinner: {home_team}")
-elif result == "Away":
-    print(f"\nWinner: {away_team}")
-else:
-    print("\nWinner: Draw")
+    winner = predict_match(home_team, away_team)
+
+    print("\n🏆 Match Prediction")
+    print(f"Home Team : {home_team}")
+    print(f"Away Team : {away_team}")
+    print(f"\nWinner: {winner}")
---
+++ src/tournament.py
@@ -0,0 +1,5 @@
+from predict import predict_match
+
+winner = predict_match("Argentina", "Japan")
+
+print(f"\nWinner: {winner}")
