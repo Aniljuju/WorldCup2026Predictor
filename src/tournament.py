13db6fb54f9521c66548e5a3af04dd1e069f1dc5
anil shrestha <stha.anill2059@gmail.com>
2026-07-14 13:29:41 +0545

    Add RO32 Prediction

--- src/tournament.py
+++ src/tournament.py
@@ -1,5 +1,8 @@
 from predict import predict_match
 
-winner = predict_match("Argentina", "Japan")
+print("🏆 FIFA World Cup 2026 - Round of 32\n")
 
-print(f"\nWinner: {winner}")
+winner = predict_match("Argentina", "Germany")
+
+print(f"\nArgentina vs Germany")
+print(f"Winner: {winner}")
