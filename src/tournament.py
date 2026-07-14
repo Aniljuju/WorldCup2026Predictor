6f986b199b5f71823578e3d4b2b07008e9a5f2ba
anil shrestha <stha.anill2059@gmail.com>
2026-07-14 13:33:44 +0545

    Automatic simulation of RO16

--- src/tournament.py
+++ src/tournament.py
@@ -23,3 +23,23 @@ for home, away in round_of_32:
 
 print("\n🏆 Teams advancing to the Round of 16")
 print(winners)
+
+print("\n🏆 Round of 16\n")
+
+round_of_16_winners = []
+
+for i in range(0, len(winners), 2):
+
+    home = winners[i]
+    away = winners[i + 1]
+
+    winner = predict_match(home, away)
+
+    print(f"{home} vs {away}")
+    print(f"Winner: {winner}")
+    print("-" * 40)
+
+    round_of_16_winners.append(winner)
+
+print("\nTeams advancing to Quarterfinals")
+print(round_of_16_winners)
