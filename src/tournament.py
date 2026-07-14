a3175d4502f908251e1f93e1286f2891c7540086
anil shrestha <stha.anill2059@gmail.com>
2026-07-14 13:31:17 +0545

    Simulate RO32

--- src/tournament.py
+++ src/tournament.py
@@ -2,7 +2,24 @@ from predict import predict_match
 
 print("🏆 FIFA World Cup 2026 - Round of 32\n")
 
-winner = predict_match("Argentina", "Germany")
+round_of_32 = [
+    ("South Africa", "Canada"),
+    ("Germany", "Paraguay"),
+    ("Netherlands", "Morocco"),
+    ("Brazil", "Japan"),
+]
 
-print(f"\nArgentina vs Germany")
-print(f"Winner: {winner}")
+winners = []
+
+for home, away in round_of_32:
+    print(f"\n{home} vs {away}")
+
+    winner = predict_match(home, away)
+
+    print(f"Winner: {winner}")
+    print("-" * 40)
+
+    winners.append(winner)
+
+print("\n🏆 Teams advancing to the Round of 16")
+print(winners)
